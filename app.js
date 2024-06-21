bd5181055318ee7b218b05b57af3d43a7e655f6b
Marllon Santos <cmarllon.santos@gmail.com>
2024-06-21 20:41:11 -0300

    Atualização no style e nas mensagens

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ function atualizarTexto(tag, texto) {
   campo = document.querySelector(tag);
   campo.innerHTML = texto;
 }
-
+function atualizarTextoID(id, texto) {
+  campo = document.getElementById(id);
+  campo.innerText = texto;
+}
 function view(alert) {
     alert.style.visibility = "visible";
+
 }
   
 function getRandomInt(max) {
@@ -20,14 +24,19 @@ function getRandomInt(max) {
 function novoNumero() {
   numeroSecreto = getRandomInt(maxNumero);
   console.log(numeroSecreto);
+  atualizarTexto('h1', 'Novo Jogo Iniciado!');
+  tentativasErradas = 0;
 }
 
 function chutar() {
+  
   if (numeroSecreto == 0) {
     novoNumero();
   }
+
   let numeroChute = document.querySelector("input").value;
   console.log(`Numero chutado: ${numeroChute}`);
+
   let mensagem = numeroChute > numeroSecreto ? 'O número é menor! <' : 'O número é maior! >';
   atualizarTexto('h1', mensagem);
 
@@ -37,8 +46,8 @@ function chutar() {
   if (numeroChute == numeroSecreto) {
     view(alertSucess);
     atualizarTexto('h1', 'uHulll!!');
+    atualizarTextoID('tentativasDiv', tentativasErradas);
   } else {
-    //view(alertErro);
     tentativasErradas = tentativasErradas + 1;
   }
 }
