b16f273b14947d1a1e20e70ec814f242c61bcf78
Marllon Santos <cmarllon.santos@gmail.com>
2024-06-21 20:22:58 -0300

    Arquivos e layout criados, assim como o game no JS

---
+++ app.js
@@ -0,0 +1,44 @@
+let numeroSecreto = 0;
+let maxNumero = 100;
+let pontuacao = 0;
+let pergunta = 0;
+let tentativasErradas = 0;
+
+function atualizarTexto(tag, texto) {
+  campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+function view(alert) {
+    alert.style.visibility = "visible";
+}
+  
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function novoNumero() {
+  numeroSecreto = getRandomInt(maxNumero);
+  console.log(numeroSecreto);
+}
+
+function chutar() {
+  if (numeroSecreto == 0) {
+    novoNumero();
+  }
+  let numeroChute = document.querySelector("input").value;
+  console.log(`Numero chutado: ${numeroChute}`);
+  let mensagem = numeroChute > numeroSecreto ? 'É menor!' : 'É maior!';
+  atualizarTexto('h1', mensagem);
+
+  if (numeroChute == "") {
+    alert("Ops! Informe um número!");
+  }
+  if (numeroChute == numeroSecreto) {
+    view(alertSucess);
+    atualizarTexto('h1', 'uHulll!!');
+  } else {
+    //view(alertErro);
+    tentativasErradas = tentativasErradas + 1;
+  }
+}
